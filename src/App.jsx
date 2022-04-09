589dca2c186137cb7854b00dab4acfc1b1a039af
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-04-09 06:23:39 +0530

    Added Datepicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import "./App.css";
 import Table from "./Table.jsx";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import DatePicker from "./DatePicker";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
 function App() {
-  let theme = createTheme({
-    typography: {},
-    palette: {
-      primary: {
-        main: "#fff",
-        // contrastText: "#fff",
-      },
-    },
-  });
+  // let theme = createTheme({
+  //   typography: {},
+  //   palette: {
+  //     primary: {
+  //       main: "#fff",
+  //       // contrastText: "#fff",
+  //     },
+  //   },
+  // });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div style={{ margin: "50px" }}>
-        <Table />
-      </div>
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    <div style={{ margin: "50px" }}>
+      <DatePicker />
+      <Table />
+    </div>
+    // </ThemeProvider>
   );
 }
 
---
+++ src/DatePicker.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+import TextField from "@mui/material/TextField";
+export default function DatePicker() {
+  const [value, setValue] = React.useState(new Date("2014-08-18T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="MM/dd/yyyy"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+    </div>
+  );
+}
